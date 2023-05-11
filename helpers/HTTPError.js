38906c468e76d419472f87ebb823b921feb142de
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-05-12 00:31:51 +0300

    update helpers

--- helpers/HTTPError.js
+++ helpers/HTTPError.js
@@ -1,4 +1,12 @@
-const HTTPError = (status, message) => {
+const errorMessageList = {
+  400: "Bad request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+};
+
+const HTTPError = (status, message = errorMessageList[status]) => {
   const error = new Error(message);
   error.status = status;
   return error;
